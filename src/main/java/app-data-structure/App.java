2a6630279939953bd9fb7f988266fc1f37f1c9a1
Vivek <vivek0811141@gmail.com>
2018-01-21 23:00:40 +0530

    Data structure with test cases

---
+++ src/main/java/app-data-structure/App.java
@@ -0,0 +1,13 @@
+package app-data-structure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
